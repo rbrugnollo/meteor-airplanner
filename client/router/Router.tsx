527317c8155b4e4d2149f9fa04deffd17a9b9904
Rafael Brugnollo <rbrugnollo@gmail.com>
2023-08-18 09:56:07 -0300

    style code

--- client/router/Router.tsx
+++ client/router/Router.tsx
@@ -4,33 +4,28 @@ import AirplaneList from "/imports/ui/airplanes/AirplaneList";
 import MainLayout from "/imports/ui/layouts/main/MainLayout";
 import { App } from "/imports/ui/App";
 import LoginForm from "/imports/ui/login/LoginForm";
-import AirplaneForm from "/imports/ui/airplanes/AirplaneForm";
-  
+
 const Router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "login",
+        element: <LoginForm />,
+      },
+      {
+        path: "app",
+        element: <MainLayout />,
         children: [
-            {
-                path: "login",
-                element: <LoginForm />
-            },
-            {
-                path: "app",
-                element: <MainLayout />,
-                children: [
-                    {
-                      path: "airplanes",
-                      element: <AirplaneList />,
-                    },
-                    {
-                        path: "addairplanes",
-                        element: <AirplaneForm />,
-                    },
-                  ],
-            }
-        ]
-    }
+          {
+            path: "airplanes",
+            element: <AirplaneList />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 export default Router;
