10f25d334da9c72cd38d0bbc392e9c5d4f39586b
Rafael Brugnollo <rbrugnollo@gmail.com>
2023-08-18 09:55:55 -0300

    Create shared Edit and Delete Btns

---
+++ imports/ui/shared/editBtn/EditBtn.tsx
@@ -0,0 +1,20 @@
+import { Tooltip, IconButton } from "@chakra-ui/react";
+import React from "react";
+import { FaPenToSquare } from "react-icons/fa6";
+
+interface EditBtnProps {
+  readonly onClick: () => void;
+}
+
+const EditBtn = ({ onClick }: EditBtnProps) => (
+  <Tooltip label="Edit">
+    <IconButton
+      aria-label="Edit"
+      icon={<FaPenToSquare />}
+      colorScheme="teal"
+      onClick={onClick}
+    />
+  </Tooltip>
+);
+
+export default EditBtn;
