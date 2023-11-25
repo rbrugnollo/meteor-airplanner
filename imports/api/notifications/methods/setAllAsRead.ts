7cccec14fc811095dec2288d2b93502cf43b568b
Rafael Brugnollo <rbrugnollo@gmail.com>
2023-11-25 14:00:36 -0300

    Add notification count and update dependent collections

--- imports/api/notifications/methods/setAllAsRead.ts
+++ imports/api/notifications/methods/setAllAsRead.ts
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { createMethod } from 'meteor/zodern:relay';
 import { z } from 'zod';
 import { NotificationsCollection } from '../collection';
@@ -6,11 +7,30 @@ export const setAllAsRead = createMethod({
   name: 'notifications.setAllAsRead',
   schema: z.undefined(),
   async run() {
-    if (this.userId)
-      await NotificationsCollection.updateAsync(
-        { userId: this.userId },
-        { $set: { read: true } },
-        { multi: true },
-      );
+    if (!this.userId) return;
+
+    await NotificationsCollection.updateAsync(
+      { userId: this.userId },
+      { $set: { read: true } },
+      { multi: true },
+    );
+    // Update dependent collections
+    await setNotificationCountToZero();
+  },
+});
+
+export const setNotificationCountToZero = createMethod({
+  name: 'notifications.setNotificationCountToZero',
+  schema: z.undefined(),
+  async run() {
+    if (!this.userId) return;
+    await Meteor.users.updateAsync(
+      { _id: this.userId },
+      {
+        $set: {
+          'profile.notificationCount': 0,
+        },
+      },
+    );
   },
 });
--- imports/ui/layouts/main/Notifications.tsx
+++ imports/ui/layouts/main/Notifications.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaBell } from 'react-icons/fa6';
-import { Badge, IconButton } from '@mui/material';
+import { Badge, IconButton, Tooltip } from '@mui/material';
+import { useLoggedUser } from 'meteor/quave:logged-user-react';
 
 const Notifications = () => {
   const navigate = useNavigate();
+  const { loggedUser } = useLoggedUser();
   return (
-    <Badge overlap="circular" badgeContent={4} color="secondary">
-      <IconButton
-        onClick={() => {
-          navigate('/app/notifications');
-        }}
-        color="primary"
-        aria-label="notifications"
-      >
-        <FaBell />
-      </IconButton>
+    <Badge
+      overlap="circular"
+      badgeContent={loggedUser?.profile?.notificationCount}
+      color="secondary"
+    >
+      <Tooltip title="Notificações">
+        <IconButton
+          onClick={() => {
+            navigate('/app/notifications');
+          }}
+          color="primary"
+          aria-label="notifications"
+        >
+          <FaBell />
+        </IconButton>
+      </Tooltip>
     </Badge>
   );
 };
